2d96e557c61d46ebeacb3d24dfc3cd6708a0766a
Viktor <viktorius488@gmail.com>
2020-07-13 12:27:08 +0300

    tracking foreground service configured | etc

--- app/src/main/java/com/viktorapps/runningtracker/activity/MainActivity.kt
+++ app/src/main/java/com/viktorapps/runningtracker/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.viktorapps.runningtracker.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.viktorapps.runningtracker.R
+import com.viktorapps.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -28,6 +32,17 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
-        
+
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent!!)
     }
 }
